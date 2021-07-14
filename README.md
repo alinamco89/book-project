9d37e2fc7c66904634ec1062354c80942d0fe5fe
alinamco89 <alicia_198929@yahoo.com>
2021-07-14 15:09:45 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-hsrnrx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-hsrnrx)
---
+++ script.js
@@ -0,0 +1,121 @@
+//Selectors
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const submitBook = document.querySelector('.submit-book');
+const bookList = document.querySelector('.book-list');
+
+//Event Listeners
+document.addEventListener('DOMContentLoaded', getBooks);
+submitBook.addEventListener('click', addBook);
+bookList.addEventListener('click', deleteBook);
+
+//Functions
+function addBook(event) {
+  //Stop browser refresh when submit the button
+  event.preventDefault();
+
+  //Create the single book div
+  const book = document.createElement('div');
+  book.classList.add('book');
+
+  //Create the Li
+  const newBook = document.createElement('li');
+  newBook.innerText =
+    toTitleCase(title.value) + ' by ' + toTitleCase(author.value);
+
+  newBook.classList.add('book-item');
+  book.appendChild(newBook);
+  //ADD BOOK TO localStorage
+  saveBooks(newBook.innerText);
+
+  //Create the DELETE button
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = '<i class="fas fa-backspace"></i>';
+  deleteBtn.classList.add('delete-btn');
+  book.appendChild(deleteBtn);
+
+  //Append to the book list
+  bookList.appendChild(book);
+
+  //Clear title and author input value
+  title.value = '';
+  author.value = '';
+  console.log(newBook.innerText);
+}
+
+//Convert the title and author to CAPITALIZE
+function toTitleCase(str) {
+  return str.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
+
+function deleteBook(e) {
+  const item = e.target;
+  //DELETE BOOK
+  if (item.classList[0] === 'delete-btn') {
+    const bookItem = item.parentElement;
+
+    //Animation
+    bookItem.classList.add('slide');
+    removeBooks(bookItem);
+    bookItem.addEventListener('transitionend', function() {
+      bookItem.remove();
+    });
+  }
+}
+
+function saveBooks(newBook) {
+  //CHECK IF I HAVE THINGS IN THERE
+
+  let books;
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+  books.push(newBook);
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+function getBooks() {
+  let books;
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+
+  books.forEach(function(bookListing) {
+    //Create the single book div
+    const book = document.createElement('div');
+    book.classList.add('book');
+
+    //Create the Li
+    const newBook = document.createElement('li');
+    newBook.innerText = bookListing;
+    newBook.classList.add('book-item');
+    book.appendChild(newBook);
+
+    //Create the DELETE button
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = '<i class="fas fa-backspace"></i>';
+    deleteBtn.classList.add('delete-btn');
+    book.appendChild(deleteBtn);
+
+    //Append to the book list
+    bookList.appendChild(book);
+  });
+}
+
+function removeBooks(bookItem) {
+  let books;
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+  const bookIndex = bookItem.children[0].innerText;
+  books.splice(books.indexOf(bookIndex), 1);
+  localStorage.setItem('books', JSON.stringify(books));
+}
